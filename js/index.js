56625c856ad037302f20fab92beb87886c93ade9
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2021-03-31 19:08:58 -0300

    fineshed layout mobile

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@ const headerContent = document.querySelector('.header-content');
 const menu = document.querySelector('.menu');
 const button = document.querySelector('.btn-mobile');
 
-console.log(headerContent, menu, button);
-
 button.addEventListener('click', handleClick);
 
 function handleClick() {
